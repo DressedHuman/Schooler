0f517a22760abc201aefce64bd72a37ad481bfe3
Motiur Rahman Mizan <dressed.human@gmail.com>
2025-07-23 17:14:20 +0600

    reinitialized the project from scratch using typescript

--- src/pages/Student/Student.jsx
+++
@@ -1,9 +0,0 @@
-const Student = () => {
-    return (
-        <div>
-            <h2 className="text-3xl font-medium">This is student page</h2>
-        </div>
-    );
-};
-
-export default Student;
